1a4f99e01b80127a2a8c2f7510e9540f2de9c8f1
Tremendous1192 <tremendous.duck@gmail.com>
2023-01-21 07:02:19 +0900

    配列を返すToArrayメソッドを追加しました。

---
+++ MatrixSharp/Source/Basic/Matrix/Methods/ToArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Tremendous1192.SelfEmployed.MatrixSharp
+{
+    public unsafe partial class Matrix
+    {
+       /// <summary>
+       /// 配列の複製を返す
+       /// </summary>
+       /// <returns></returns>
+        public double[,] ToArray()
+        {
+            return (double[,])_array.Clone();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MatrixSharp
+手計算に近い感覚で行列とベクトルの基本的な計算を行うことを目指したパッケージです。
+
+
+[詳細をZenn](https://zenn.dev/tremendous1192/articles/824b2d32381173)しておりますので、そちらをご確認頂けますと幸いです。
+
+[GitHub](https://github.com/Tremendous1192/MatrixSharp)
+
+[Nuget](https://www.nuget.org/packages/MatrixSharp/)
