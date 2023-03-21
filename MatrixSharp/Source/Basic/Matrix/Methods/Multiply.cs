1247cf3598249cd12e98e34201dc54d2cac2c4e8
Tremendous1192 <tremendous.duck@gmail.com>
2023-03-21 13:42:17 +0900

    publicな掛け算メソッドの主要な計算を、internal functionで行うように変更した。

--- MatrixSharp/Source/Basic/Matrix/Methods/Multiply.cs
+++ MatrixSharp/Source/Basic/Matrix/Methods/Multiply.cs
@@ -22,8 +22,10 @@ namespace Tremendous1192.SelfEmployed.MatrixSharp
                 throw new ArgumentException("左の行列の列数と、右の行列の行数を揃えてください");
             }
 
-            double[,] rightT = right.Transpose()._array;
-            double[,] calculated = new double[left._row, right.Column];
+            //double[,] rightT = right.Transpose()._array;
+            double[,] calculated = MultiplyIJ1K1(left._array, right.Transpose()._array);
+                
+            /*    new double[left._row, right.Column];
             int lenI = left._array.GetLength(0), lenJ = right._array.GetLength(1), lenK = right._array.GetLength(0);
             fixed (double* pleft = left._array, pright = rightT, pcalculated = calculated)
             {
@@ -36,7 +38,7 @@ namespace Tremendous1192.SelfEmployed.MatrixSharp
                         *pc += *plk * *prk;
                     }
                 }
-            }
+            }*/
 
             return new Matrix(calculated, false);
         }
